ce0e766e9adf9bd2f4ad7978c55871fb57b15dfd
Justin Archinihu <justinarchinihu@MacBook-Air-269.local>
2026-06-19 12:55:22 -0400

    update from VS Code

--- app/main.py
+++ app/main.py
@@ -1543,7 +1543,32 @@ def darvin():
         songs=songs
     )
 
+    
+@app.route("/rocklee")
+def rocklee():
 
+    songs = [
+        {
+            "title": "body2",
+            "release_type": "single",
+            "filename": "Audio/body2 by rocklee.m4a"
+        },
+        {
+            "title": "body3",
+            "release_type": "single",
+            "filename": "Audio/body3 by rocklee.m4a"
+        }
+    ]
+
+    return render_template(
+        "rocklee.html",
+        artist_name="Rocklee",
+        location="Jacksonville",
+        latest_releases="body2",
+        cover_image="images/rocklee.jpg",
+        profile_image="images/rocklee.jpg",
+        songs=songs
+    )
 # =====================================
 # RUN APP
 # =====================================
